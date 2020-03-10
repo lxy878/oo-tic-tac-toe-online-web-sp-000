091782723440964e29f7e1e21c5f3a8ff1743a7e
Xiaoyi Liu <lxy878@gmail.com>
2020-03-10 10:20:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,5 @@
 class TicTacToe
-  attr_accessor :board
+  attr_accessor :board :current_player
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
   def initialize
     @board = [" "," "," "," "," "," "," "," "," "]
@@ -54,7 +54,7 @@ class TicTacToe
 
   def won?
     #check board
-    
+
     # if won, return true
     # otherwise, returne false
   end
